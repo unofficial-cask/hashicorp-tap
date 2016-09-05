ebeea6b9694285d223e105e797302e69274d1904
Radek Simko <radek.simko@gmail.com>
2016-09-05 11:02:12 +0100

    nomad 0.4.1

--- Casks/nomad.rb
+++ Casks/nomad.rb
@@ -1,6 +1,6 @@
 cask 'nomad' do
-  version '0.4.0'
-  sha256 '2f6bff636ae48343017e0e5edd4371f7abe434d1f70d95d7e28f08490c978c40'
+  version '0.4.1'
+  sha256 '5f2d52c73e992313e803fb29b6957ad1b754ed6e68bed5fa9fbe9b8e10a67aeb'
 
   url "https://releases.hashicorp.com/nomad/#{version}/nomad_#{version}_darwin_amd64.zip"
   appcast 'https://github.com/hashicorp/nomad/releases.atom',
