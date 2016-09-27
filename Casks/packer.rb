b452a870fc7789597e17843371ae45d2538f922b
stack72 <public@paulstack.co.uk>
2016-09-27 17:40:33 +0100

    Bump packer to 10.2

--- Casks/packer.rb
+++ Casks/packer.rb
@@ -1,10 +1,10 @@
 cask 'packer' do
-  version '0.10.1'
-  sha256 'fac621bf1fb43f0cbbe52481c8dfda2948895ad52e022e46f00bc75c07a4f181'
+  version '0.10.2'
+  sha256 '2ddd7a5ffe501978f4eaa5c1c5b0443556aaaa6e093b79e0670c8ea833f86d9d'
 
   url "https://releases.hashicorp.com/packer/#{version}/packer_#{version}_darwin_amd64.zip"
   appcast 'https://github.com/mitchellh/packer/releases.atom',
-          checkpoint: 'e5561e741b39748bb7f33a90122f974d19281d00ad0c768abb58294df01187fe'
+          checkpoint: '7515bf959b73b956ceb967351c7e299cbb3668a53d35f9c770eb72e00d93ced6'
   name 'Packer'
   homepage 'https://www.packer.io/'
   license :oss
