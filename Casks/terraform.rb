9412aab93a8aac55a92c28ad8ab3e0e98e155a4c
Michael Kuzmin <mkuzmin@gmail.com>
2017-01-26 02:09:47 +0300

    Fix errors for Terraform
    - incorrect SHA256
    - wrong type of quotes

--- Casks/terraform.rb
+++ Casks/terraform.rb
@@ -1,10 +1,10 @@
 cask 'terraform' do
   version '0.8.4'
-  sha256 `297d35d0b4311445cd87ef032d3dec917bcc7a8b163ead28a4c45d966a2f75cc`
+  sha256 '79e94dfaf439fdbba2a2fe03dd7c90b24efa699b6661155aa9329df43e68ba51'
 
   url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
   appcast 'https://github.com/hashicorp/terraform/releases.atom',
-          checkpoint: `4570e8f1910ebdf8196771a745ea96f0f0deebf38df9373474c532f31a17ceaf`
+          checkpoint: '4570e8f1910ebdf8196771a745ea96f0f0deebf38df9373474c532f31a17ceaf'
   name 'Terraform'
   homepage 'https://www.terraform.io/'
   license :mpl
