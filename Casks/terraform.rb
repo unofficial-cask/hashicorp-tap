45a3bb9b4a92f3ceb552aa168bc5f16862efc945
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2017-01-31 09:33:55 -0800

    terraform: update to 0.8.5

--- Casks/terraform.rb
+++ Casks/terraform.rb
@@ -1,6 +1,6 @@
 cask 'terraform' do
-  version '0.8.4'
-  sha256 '297d35d0b4311445cd87ef032d3dec917bcc7a8b163ead28a4c45d966a2f75cc'
+  version '0.8.5'
+  sha256 '10253ac843b7a170844d629cbdbd2287bf687cdd3d2938e4ab9140d10534cf38'
 
   url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
   appcast 'https://github.com/hashicorp/terraform/releases.atom',
