085c0cf813bc3e6265154b935d37fc9c66bd75ba
stack72 <public@paulstack.co.uk>
2017-03-08 15:14:39 +0200

    Bump terraform cask to 0.8.8

--- Casks/terraform.rb
+++ Casks/terraform.rb
@@ -1,10 +1,10 @@
 cask 'terraform' do
-  version '0.8.7'
-  sha256 'ba53c7424bec5db7c01e0a5178ba5e295eb13669fb04fdae41576098baf88b75'
+  version '0.8.8'
+  sha256 '55ab547539e68c9375c144062460457fcfdb3f5b9f412d3bb162f73298602d78'
 
   url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
   appcast 'https://github.com/hashicorp/terraform/releases.atom',
-          checkpoint: '4570e8f1910ebdf8196771a745ea96f0f0deebf38df9373474c532f31a17ceaf'
+          checkpoint: '2f0400fd817496eeec16d62a830f54eb16d4f3ada57654dc28047ee700427bd4'
   name 'Terraform'
   homepage 'https://www.terraform.io/'
   license :mpl
